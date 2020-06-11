0245fcbaf5701c4c036eb174f8c9a8640efc4d95
Alex231 <git@alex231.com>
2020-06-11 13:56:55 +0100

    1.0.2

--- src/gg/codie/mineonline/Globals.java
+++ src/gg/codie/mineonline/Globals.java
@@ -2,7 +2,7 @@ package gg.codie.mineonline;
 
 public class Globals {
 
-    public static final String LAUNCHER_VERSION = "1.0.1";
+    public static final String LAUNCHER_VERSION = "1.0.2";
     public static final String API_HOSTNAME = "mineonline.codie.gg";
 
 }
