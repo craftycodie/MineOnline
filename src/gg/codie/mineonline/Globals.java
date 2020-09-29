8fd045340ca3dfad74691b9b638cb8989af4a944
Alex231 <git@alex231.com>
2020-09-29 09:21:13 +0100

    Removed old urls from globals.

--- src/gg/codie/mineonline/Globals.java
+++ src/gg/codie/mineonline/Globals.java
@@ -6,32 +6,4 @@ public class Globals {
     public static final boolean DEV = false;
     public static final String API_HOSTNAME = "mineonline.codie.gg";
     public static final String DISCORD_APP_ID = "718163542783819818";
-    public static final String[] REDIRECTED_DOMAINS = new String[] {
-            "textures.minecraft.net",
-            "pc.realms.minecraft.net",
-            "www.minecraft.net:-1",
-            "skins.minecraft.net",
-            "session.minecraft.net",
-            "realms.minecraft.net",
-            "assets.minecraft.net",
-            "mcoapi.minecraft.net",
-            "snoop.minecraft.net",
-            "www.minecraft.net",
-            "resources.download.minecraft.net",
-            "libraries.minecraft.net",
-            "minecraft.net",
-            "s3.amazonaws.com",
-            "api.mojang.com",
-            "account.mojang.com",
-            "authserver.mojang.com",
-            "sessionserver.mojang.com",
-            "launchermeta.mojang.com",
-            "mojang.com",
-            "aka.ms",
-
-            // for mods
-            "banshee.alex231.com",
-            "mcauth-alex231.rhcloud.com",
-    };
-
 }
