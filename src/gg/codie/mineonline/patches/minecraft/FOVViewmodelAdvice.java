9ecdaf77e7384f323ed75ce0bbe017cfcd2ba1b1
Alex231 <git@alex231.com>
2020-10-15 14:41:40 +0100

    Added a1.2.6 viewmodel fix info.

--- src/gg/codie/mineonline/patches/minecraft/FOVViewmodelAdvice.java
+++ src/gg/codie/mineonline/patches/minecraft/FOVViewmodelAdvice.java
@@ -14,13 +14,10 @@ public class FOVViewmodelAdvice {
 
             GL11.glMatrixMode(5889);
             GL11.glLoadIdentity();
-//            float temp = this.j.z.fov;
-//            this.j.z.fov = 0.0f;
             ClassLoader.getSystemClassLoader().loadClass("gg.codie.mineonline.patches.lwjgl.LWJGLPerspectiveAdvice").getField("ignore").set(null, true);
             GLU.gluPerspective(originalFOV, (float) Display.getWidth() / Display.getHeight(), 0.05f, zFar);
             ClassLoader.getSystemClassLoader().loadClass("gg.codie.mineonline.patches.lwjgl.LWJGLPerspectiveAdvice").getField("ignore").set(null, false);
 
-//            this.j.z.fov = temp;
             GL11.glMatrixMode(5888);
             GL11.glLoadIdentity();
         } catch (Exception ex) {
