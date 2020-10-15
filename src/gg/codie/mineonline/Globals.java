88da81e257f0ad69fa53a6d4e918bd697e1d6024
Alex231 <git@alex231.com>
2020-10-15 15:59:05 +0100

    Removed USE_MOJAMG_API from globals.

--- src/gg/codie/mineonline/Globals.java
+++ src/gg/codie/mineonline/Globals.java
@@ -1,10 +1,9 @@
 package gg.codie.mineonline;
 
 public class Globals {
-    public static final String LAUNCHER_VERSION = "#130 Test Build 1";
+    public static final String LAUNCHER_VERSION = "#130 Test Build 2";
     public static final boolean DEV = true;
     public static final String API_HOSTNAME = "mineonline.codie.gg";
     public static final String API_PROTOCOL = "https://";
     public static final String DISCORD_APP_ID = "718163542783819818";
-    public static boolean USE_MOJANG_API = true;
 }
