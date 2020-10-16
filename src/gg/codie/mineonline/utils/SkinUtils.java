4e0c2cc972d288db9938693027ab672bbebc9339
Alex231 <git@alex231.com>
2020-10-16 18:58:28 +0100

    Removed legacy minecraft capes code.

--- src/gg/codie/mineonline/utils/SkinUtils.java
+++ src/gg/codie/mineonline/utils/SkinUtils.java
@@ -3,7 +3,6 @@ package gg.codie.mineonline.utils;
 import gg.codie.minecraft.api.MojangAPI;
 import gg.codie.minecraft.api.SessionServer;
 import gg.codie.mineonline.Globals;
-import gg.codie.mineonline.LauncherFiles;
 import gg.codie.mineonline.Settings;
 import org.json.JSONObject;
 
@@ -11,8 +10,6 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.util.Base64;
 
 public class SkinUtils {
@@ -34,67 +31,17 @@ public class SkinUtils {
         }
     }
 
-    public static byte[] minecraftCapesCape(String uuid) throws IOException {
+    public static boolean hasCustomCape(String uuid) throws IOException {
         HttpURLConnection connection;
 
-        URL url = new URL("https://minecraftcapes.net/profile/" + uuid);
+        URL url = new URL(Globals.API_PROTOCOL + Globals.API_HOSTNAME + "/api/player/" + uuid + "/customcape");
         connection = (HttpURLConnection) url.openConnection();
         connection.setRequestProperty("Content-Type", "application/json");
         connection.setRequestMethod("GET");
         connection.setDoInput(true);
         connection.setDoOutput(false);
 
-        InputStream is = connection.getInputStream();
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-
-        StringBuilder response = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            response.append(line);
-            response.append('\r');
-        }
-        rd.close();
-
-        try {
-            JSONObject res = new JSONObject(response.toString());
-            return Base64.getDecoder().decode(res.getJSONObject("textures").getString("cape"));
-        } catch (Exception ex) {
-            if (Globals.DEV)
-                ex.printStackTrace();
-            return null;
-        }
-    }
-
-    @Deprecated
-    public static String minecraftCapesCapeUrl(String uuid) throws IOException {
-        HttpURLConnection connection;
-
-        URL url = new URL("https://minecraftcapes.net/profile/" + uuid);
-        connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setRequestMethod("GET");
-        connection.setDoInput(true);
-        connection.setDoOutput(false);
-
-        InputStream is = connection.getInputStream();
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-
-        StringBuilder response = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            response.append(line);
-            response.append('\r');
-        }
-        rd.close();
-
-        try {
-            JSONObject res = new JSONObject(response.toString());
-            return res.getString("cape");
-        } catch (Exception ex) {
-            if (Globals.DEV)
-                ex.printStackTrace();
-            return null;
-        }
+        return connection.getResponseCode() == 200;
     }
 
     public static String findCloakURLForUsername(String username) {
@@ -104,23 +51,8 @@ public class SkinUtils {
                 throw new FileNotFoundException("User not found: " + username);
 
             if (Settings.settings.optBoolean(Settings.CUSTOM_CAPES, false)) {
-//                byte[] capePng = minecraftCapesCape(profile.getString("id"));
-//
-//                if (capePng != null) {
-//                    File capeFile = new File(LauncherFiles.MINEONLINE_TEMP_CAPES_FOLDER + profile.getString("id") + ".png");
-//                    if (capeFile.exists())
-//                        capeFile.delete();
-//
-//                    capeFile.mkdirs();
-//                    capeFile.createNewFile();
-//                    Files.write(capeFile.toPath(), capePng, StandardOpenOption.WRITE);
-//                    capeFile.deleteOnExit();
-//
-//                    return capeFile.toURI().toURL().toString();
-//                }
-                String capeUrl = minecraftCapesCapeUrl(profile.getString("id"));
-                if (capeUrl != null)
-                    return capeUrl;
+                if (hasCustomCape(profile.getString("id")))
+                    return Globals.API_PROTOCOL + Globals.API_HOSTNAME + "/api/player/" + profile.getString("id") + "/customcape";
             }
 
             profile = SessionServer.minecraftProfile(profile.getString("id"));
@@ -154,22 +86,8 @@ public class SkinUtils {
     public static String findCloakURLForUuid(String uuid) {
         try {
             if (Settings.settings.optBoolean(Settings.CUSTOM_CAPES, false)) {
-//                byte[] capePng = minecraftCapesCape(uuid);
-//
-//                if (capePng != null) {
-//                    File capeFile = new File(LauncherFiles.MINEONLINE_TEMP_CAPES_FOLDER + uuid + ".png");
-//                    if (capeFile.exists())
-//                        capeFile.delete();
-//                    capeFile.getParentFile().mkdirs();
-//                    Files.write(capeFile.toPath(), capePng, StandardOpenOption.CREATE_NEW);
-//                    capeFile.deleteOnExit();
-//
-//                    return capeFile.toURI().toURL().toString();
-//                }
-
-                String capeUrl = minecraftCapesCapeUrl(uuid);
-                if (capeUrl != null)
-                    return capeUrl;
+                if (hasCustomCape(uuid))
+                    return Globals.API_PROTOCOL + Globals.API_HOSTNAME + "/api/player/" + uuid + "/customcape";
             }
 
             JSONObject profile = SessionServer.minecraftProfile(uuid);
