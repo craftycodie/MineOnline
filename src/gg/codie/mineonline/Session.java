fdf44d271775bd37d3bf2dfba0aa6e86f733b3cd
Codie Newark <git@codie.gg>
2021-07-07 22:25:48 +0100

    Removed an old skin cache reducing load times.

--- src/gg/codie/mineonline/Session.java
+++ src/gg/codie/mineonline/Session.java
@@ -52,7 +52,6 @@ public class Session {
         this.username = username;
         this.uuid = UUID.randomUUID().toString();
         this.isPremium = false;
-        cacheSkin();
     }
 
     public Session(String username, String accessToken, String clientToken, String uuid, boolean isPremium) {
@@ -66,88 +65,11 @@ public class Session {
         this.accessToken = accessToken;
         this.uuid = uuid;
         this.isPremium = isPremium;
-        cacheSkin();
         DiscordRPCHandler.updateSession(username);
     }
 
     public void logout() {
         session = null;
         LastLogin.deleteLastLogin();
-    }
-
-    public void cacheSkin () {
-        (new Thread() {
-            public void run() {
-                Settings.singleton.loadSettings();
-
-                String skinUrl = SkinUtils.findSkinURLForUuid(uuid);
-
-                try (BufferedInputStream in = new BufferedInputStream(new URL(skinUrl).openStream())) {
-
-                    // Delete the currently cached skin.
-                    File cachedSkin = new File(LauncherFiles.CACHED_SKIN_PATH);
-                    if (cachedSkin.exists()) {
-                        cachedSkin.delete();
-                    }
-
-                    FileOutputStream fileOutputStream = new FileOutputStream(LauncherFiles.CACHED_SKIN_PATH);
-                    byte dataBuffer[] = new byte[2048];
-                    int bytesRead;
-                    while ((bytesRead = in.read(dataBuffer, 0, 2048)) != -1) {
-                        fileOutputStream.write(dataBuffer, 0, bytesRead);
-                    }
-
-                    fileOutputStream.close();
-
-
-                } catch (IOException e) {
-                    File cachedSkin = new File(LauncherFiles.CACHED_SKIN_PATH);
-                    if (cachedSkin.exists()) {
-                        cachedSkin.delete();
-                    }
-                }
-
-                String cloakUrl = SkinUtils.findCloakURLForUuid(uuid);
-
-                try (BufferedInputStream in = new BufferedInputStream(new URL(cloakUrl).openStream())) {
-
-                    // Delete the currently cached skin.
-                    File cachedCloak = new File(LauncherFiles.CACHED_CLOAK_PATH);
-                    if (cachedCloak.exists()) {
-                        cachedCloak.delete();
-                    }
-
-                    FileOutputStream fileOutputStream = new FileOutputStream(LauncherFiles.CACHED_CLOAK_PATH);
-
-                    byte dataBuffer[] = new byte[2048];
-                    int bytesRead;
-                    while ((bytesRead = in.read(dataBuffer, 0, 2048)) != -1) {
-                        fileOutputStream.write(dataBuffer, 0, bytesRead);
-                    }
-
-                    fileOutputStream.close();
-
-                } catch (IOException e) {
-                    // handle exception
-                    File cachedCloak = new File(LauncherFiles.CACHED_CLOAK_PATH);
-                    if (cachedCloak.exists()) {
-                        cachedCloak.delete();
-                    }
-                }
-
-                try {
-                    JSONObject skinMetadata = SkinUtils.getSkinMetadata(uuid);
-
-                    FileWriter fileWriter = new FileWriter(LauncherFiles.CACHED_SKIN_METADATA_PATH, false);
-                    fileWriter.write(skinMetadata.toString());
-                    fileWriter.close();
-                } catch (IOException io) {
-                    File cachedMetadata = new File(LauncherFiles.CACHED_SKIN_METADATA_PATH);
-                    if (cachedMetadata.exists()) {
-                        cachedMetadata.delete();
-                    }
-                }
-            }
-        }).start();
     }
 }
