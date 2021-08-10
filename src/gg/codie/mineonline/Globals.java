64f4454bddcfafa52e4de63236ec8f674c8c210b
Codie Newark <git@codie.gg>
2021-08-10 20:18:18 +0100

    4.1.2

--- src/gg/codie/mineonline/Globals.java
+++ src/gg/codie/mineonline/Globals.java
@@ -1,7 +1,7 @@
 package gg.codie.mineonline;
 
 public class Globals {
-    public static final String LAUNCHER_VERSION = "4.1.1";
+    public static final String LAUNCHER_VERSION = "4.1.2";
     public static final boolean DEV = true;
     public static final String DISCORD_APP_ID = "718163542783819818";
     public static final String BRANCH = "release";
