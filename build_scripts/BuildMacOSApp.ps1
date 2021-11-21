0808d706c30fb62d1c1404fb8431728b06cec486
Codie Stella <git@codie.gg>
2021-11-21 17:29:05 +0000

    ...

--- build_scripts/BuildMacOSApp.ps1
+++ build_scripts/BuildMacOSApp.ps1
@@ -1,6 +1,6 @@
 $pomVersion = ([xml](Get-Content .\pom.xml)).project.version
 ((Get-Content -path .\build_scripts\MineOnline.app\Contents\info.plist -Raw) -replace '{version}', $pomVersion) | Set-Content -Path .\build_scripts\MineOnline.app\Contents\info.plist
-cp ".\target\MineOnline-$pomVersion.jar" ".\build_scripts\MineOnline.app\Contents\Java\MineOnline-$pomVersion.jar"
+cp "target/MineOnline-$pomVersion.jar" "build_scripts/MineOnline.app/Contents/Java/MineOnline-$pomVersion.jar"
 $compress = @{
   Path = ".\build_scripts\MineOnline.app"
   CompressionLevel = "Fastest"
