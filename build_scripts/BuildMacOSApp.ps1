8a3ce1243c1700ba68c3f961f522cf0623f64d8c
Codie Stella <git@codie.gg>
2021-11-21 16:53:17 +0000

    Fixed another typo.

--- build_scripts/BuildMacOSApp.ps1
+++ build_scripts/BuildMacOSApp.ps1
@@ -1,5 +1,5 @@
 $pomVersion = ([xml](Get-Content .\pom.xml)).project.version
-((Get-Content -path .\build_scripts\MineOnline.app\Contents\Info.plist -Raw) -replace '{version}', $pomVersion) | Set-Content -Path .\build_scripts\MineOnline.app\Contents\info.plist
+((Get-Content -path .\build_scripts\MineOnline.app\Contents\info.plist -Raw) -replace '{version}', $pomVersion) | Set-Content -Path .\build_scripts\MineOnline.app\Contents\info.plist
 Copy-Item ".\target\MineOnline-$pomVersion.jar" -Destination ".\build_scripts\MineOnline.app\Contents\Java"
 $compress = @{
   Path = ".\build_scripts\MineOnline.app"
