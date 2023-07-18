90ec09f7b401d6e3bc4a53b49e2c2ff21ce281ef
Codie Stella <git@codie.gg>
2023-07-18 18:20:14 +0100

    Update GuiMainMenu.java

--- src/main/java/gg/codie/mineonline/gui/screens/GuiMainMenu.java
+++ src/main/java/gg/codie/mineonline/gui/screens/GuiMainMenu.java
@@ -150,7 +150,7 @@ public class GuiMainMenu extends AbstractGuiScreen
         else if (OSUtils.isM1JVM())
             Font.minecraftFont.drawString("x86 JVM Recommended!", 2, getHeight() - 20, 0xffff00);
         Font.minecraftFont.drawString("MineOnline " + (Globals.DEV ? "Dev " : "") + Globals.LAUNCHER_VERSION + (!Globals.BRANCH.equalsIgnoreCase("main") ? " (" + Globals.BRANCH + ")" : ""), 2, getHeight() - 10, 0xffffff);
-        String s = "Made by @craftycodie <3";
+        String s = "Made by @codiegotlost <3";
         Font.minecraftFont.drawString(s, getWidth() - Font.minecraftFont.width(s) - 2, getHeight() - 10, 0xffffff);
         super.drawScreen(mouseX, mouseY);
     }
